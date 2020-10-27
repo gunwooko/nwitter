06aaa5ae20c1ac60cae1cd8b9c531c00e41fff80
gunwooko <kkw722@gmail.com>
2020-10-27 23:36:44 +0900

    Authentication with firbase signUp, logIn, logOut, socialLogIn

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // Response from Firebase is late, so in this hook We can check if it is logged in or not
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
+
   return (
     <>
-      <AppRouter isLoggedIn={isLoggedIn} />
+      {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
       <footer> &copy; Nwitter {new Date().getFullYear()}</footer>
     </>
   );
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,20 @@
 import React from "react";
+import { useHistory, useHistoy } from "react-router-dom";
+import { authService } from "fbase";
 
-const Profile = () => <span>Profile</span>;
+const Profile = () => {
+  const history = useHistory();
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
